74e0bf9f4c8dece03218eaad008e0a0e8bd2dc7f
alex4thenguyen <57741006+alex4thenguyen@users.noreply.github.com>
2023-06-29 18:02:19 -0400

    Add files via upload
    You can open "insert.sql" in any SQL IDE and run the script to populate the 'LOGIN' and 'PARTICIPANT' tables with required test data.

---
+++ insert.sql
@@ -0,0 +1,13 @@
+use DemoDayJudging; --Using this database
+
+insert into LOGIN(username, password, admin, judge) values('anguyen127', 'su', 'YES', 'NO');
+insert into LOGIN(username, password, admin, judge) values('hbae9', 'su', 'YES', 'NO');
+insert into LOGIN(username, password, admin, judge) values('mharry4', 'su', 'NO', 'YES');
+insert into LOGIN(username, password, admin, judge) values('kajibade1', 'su', 'NO', 'YES');
+insert into LOGIN(username, password, admin, judge) values('pdhupia1', 'su', 'NO', 'YES');
+
+insert into PARTICIPANT(participantID, fName, lName, project) values(0001, 'Alpha', 'Alpha', 'ProjA');
+insert into PARTICIPANT(participantID, fName, lName, project) values(0002, 'Beta', 'Beta', 'ProjB');
+insert into PARTICIPANT(participantID, fName, lName, project) values(0003, 'Gamma', 'Gamma', 'ProjC');
+insert into PARTICIPANT(participantID, fName, lName, project) values(0004, 'Delta', 'Delta', 'ProjD');
+insert into PARTICIPANT(participantID, fName, lName, project) values(0005, 'Epsilon', 'Epsilon', 'ProjE');
