664674dd852136bbaecfc725771fad11319d0b52
Olivia <hyunjubae0307@gmail.com>
2023-06-29 16:10:12 -0400

    Participant List HTML, CSS, Javascript

---
+++ front/Participant List View/participant_list.js
@@ -0,0 +1,40 @@
+//Participant data array
+//need to connect to database later
+var participants = [
+  { name: "Participant1", projectName: "Machine Learning", profile: "Lorem ipsum dolor sit amet..." },
+  { name: "Participant2", projectName: "Programming Language Concept", profile: "Lorem ipsum dolor sit amet..." },
+  // Add more participants as needed
+];
+
+var table = document.getElementById("participantTable");
+
+// Loop through participants and populate the table
+for (var i = 0; i < participants.length; i++) {
+  var participant = participants[i];
+
+  // Create a new row
+  var row = table.insertRow();
+
+  // Insert name column
+  var nameCell = row.insertCell();
+  nameCell.innerHTML = participant.name;
+
+  // Insert project name column
+  var projectNameCell = row.insertCell();
+  projectNameCell.innerHTML = participant.projectName;
+
+  // Insert evaluate column
+  var evaluateCell = row.insertCell();
+  var evaluateButton = document.createElement("button");
+  evaluateButton.innerHTML = "Evaluate";
+  evaluateButton.addEventListener("click", createProfileWindow(participant.profile));
+  evaluateCell.appendChild(evaluateButton);
+}
+
+// Function to open a new window with the participant's profile
+function createProfileWindow(profile) {
+  return function() {
+    var profileWindow = window.open("", "_blank", "width=400,height=1000");
+    profileWindow.document.write("<html><body><h2>Participant Profile</h2><p>" + profile + "</p></body></html>");
+  };
+}
