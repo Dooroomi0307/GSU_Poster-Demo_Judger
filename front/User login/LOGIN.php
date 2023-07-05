0b27cadcf132fff4e07576a9f5eaee301e8a4c18
Kikiyin Ajibade <111385309+kiki0426@users.noreply.github.com>
2023-07-05 16:46:01 -0400

    updated  LOGIN.php

--- front/User login/LOGIN.php
+++ front/User login/LOGIN.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
    
     <title>Login</title>
     <link rel="stylesheet" href="LOGIN.css">
@@ -14,13 +13,13 @@
 </header>
 
     <form action="signin.php" method="post">
-	
 		<h2> LOGIN </h2>
-		<?php if (isset($_GET['error' ])){ ?>
-			<p class="error"><?php echo $_GET['error' ]; ?> </p>
+		
+		<?php if (isset($_GET['error'])){ ?>
+			<p class="error"> <?php echo $_GET['error']; ?> </p>
 		<?php }	?>
 		<label> User Name</label>
-		<input type="text" name="uname" placeholder="User Name"><br>
+		<input type="text" name="uname" placeholder="UserName"><br>
 		
 		<label> Password </label>
 		<input type="password" name="password" placeholder="Password"><br>
