2d0f34496f47748b86dd9243124e5e5b9b9fe6ba
Kikiyin Ajibade <111385309+kiki0426@users.noreply.github.com>
2023-07-05 16:47:44 -0400

    added samepage error warning signin.php

--- front/User login/signin.php
+++ front/User login/signin.php
@@ -1,36 +1,52 @@
 <?php
+$uname = $_POST['uname'];
+$pass = $_POST['password'];
 
-include("config.php");
-include("FIREBASE.php");
+$validForm = true;
 
-
-
-	$uname = ($_POST['uname']);
-	$pass = ($_POST['password']);
-	
-	if(empty($uname)){
-		echo "Email is required";
-	}else if (empty($pass)){
-		echo "password is required"
-	}else{
-		$rdb = new FIREBASE($databaseURL);
-		$retrieve = $rdb->retrieve("/Login","/username", "EQUAL",$uname);
-		$data = json_decode($retrieve, 1);
-		
-		if(count($data == 0)){
-			echo "Username not registered";
-		}else{
-			$id = array_keys($data)[0];
-			if($data[id]['password'] == $password){
-				echo "Login sucess";
-			}else{
-				echo "Login failed";
-			}
-			
-			
-	}
-	
-	
+if (empty($uname) || empty($pass)) {
+    ob_end_clean();
+    header("Location: LOGIN.php?error=All fields are required");
+    exit();
+    $validForm = false; // This line is unreachable and can be removed
 }
+
+if ($validForm) {
+    $file = fopen("users.txt", "r") or die("Unable to open file!");
+    $loginFound = false;
+    $adminpage = false;
+
+    while (($line = fgets($file)) !== false) {
+        $login = explode(",", $line);
+
+        if (trim($login[1]) === $uname && trim($login[2]) === $pass) {
+            if (trim($login[1]) === "admin") { // Added missing closing double quotes
+                $adminpage = true;
+                break;
+            }
+
+            $loginFound = true;
+            break;
+        }
+    }
+
+    fclose($file);
+
+    if ($loginFound) {
+        ob_end_clean();
+        header("Location: https://gsu-demo-day.web.app/");
+        exit();
+    } else {
+        ob_end_clean();
+        header("Location: LOGIN.php?error=Incorrect Username or Password");
+        exit();
+    }
+} else {
+    ob_end_clean();
+    header("Location: LOGIN.php?error=Error opening file");
+    exit();
+}
+
+
 
 ?>
