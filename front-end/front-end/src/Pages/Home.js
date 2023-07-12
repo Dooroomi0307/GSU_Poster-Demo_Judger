2372090fe6088b87588cfa9c36fccbaa844e4703
kiki0426 <111385309+kiki0426@users.noreply.github.com>
2023-07-12 13:08:01 -0400

    correct login

--- front-end/front-end/src/Pages/Home.js
+++
@@ -1,5 +0,0 @@
-function Home(){
-  return <h1>Temporary Home</h1>
-}
-
-export default Home;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import './elements/App.css'
-<<<<<<< HEAD
+
 import Navbar from "./elements/Navbar.js"
 import Home from "./Pages/Home.js"
 import Login from "./Pages/Login/Login.jsx"
@@ -11,48 +11,7 @@ import Vote from "./Pages/Vote/Vote.js"
 import { Route, Routes } from "react-router-dom"
 
 function App() {
- 
-=======
-import db from "./firebase"
-import { useState, useEffect } from 'react'
-import Navbar from "./elements/Navbar"
-import Home from "./Pages/Home"
-import Login from "./Pages/Login/Login"
-import ParticipantList from "./Pages/ParticipantList/ParticipantList"
-import Evaluate from "./Pages/Evaluate/Evaluate"
-import Analysis from "./Pages/Analysis/Analysis"
-import Vote from "./Pages/Vote/Vote"
-import Evaluation from './Pages/Evaluate/Evaluation';
-import { Route, Routes, Router} from "react-router-dom"
 
-function App() {
-
-  const [selectedParticipant, setSelectedParticipant] = useState('');
-
-  const openEvaluationModal = (participantName) => {
-    setSelectedParticipant(participantName);
-  };
-
-  const submitEvaluation = (participant, score) => {
-    db.collection('Analysis')
-      .doc(participant)
-      .set({
-        Score: score,
-      })
-      .then(() => {
-        alert('Evaluation submitted successfully!');
-        setSelectedParticipant('');
-      })
-      .catch((error) => {
-        console.error('Error submitting evaluation:', error);
-        alert('Failed to submit evaluation. Please try again.');
-      });
-  };
-
-    
-  
-
->>>>>>> c86f8b5a49a305741007353c8d94febb4f3bab4e
   //use capital letter for component name
   return(
     <>
